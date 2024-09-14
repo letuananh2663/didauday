1af26bcc513161fe2d1faedfa55dbb5726182647
tuananh <letuananh2663@gmail.com>
2024-09-14 15:18:48 +0700

    Header and Authentication

---
+++ app/(landingpage)/header.tsx
@@ -0,0 +1,49 @@
+import {
+    ClerkLoaded,
+    ClerkLoading,
+    SignInButton,
+    SignedIn,
+    SignedOut,
+    UserButton,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
+export const Header = () => {
+
+    return (
+        <header className="h-20 w-full">
+            <div className="flex h-full items-center justify-between  bg-red-400">
+                <Link href="/" className="flex items-center gap-x-3 pb-7 pl-4 pt-8">
+                    <Image src="/logo.svg" alt="Di Dau Day" height={160} width={160} />
+                </Link>
+
+                <div className="flex gap-x-3 pr-4">
+                    <ClerkLoading>
+                        <Loader className="h-5 w-5 animate-spin text-muted-foreground" />
+                    </ClerkLoading>
+                    <ClerkLoaded>
+                        <SignedIn>
+                            <UserButton />
+                        </SignedIn>
+
+                        <SignedOut>
+                            <SignInButton
+                                mode="modal"
+                                fallbackRedirectUrl="/learn"
+                                signUpFallbackRedirectUrl="/learn"
+                            >
+                                <Button size="lg" variant="ghost">
+                                    Đăng nhập
+                                </Button>
+                            </SignInButton>
+                        </SignedOut>
+                    </ClerkLoaded>
+                </div>
+            </div>
+        </header >
+    );
+};
---
+++ app/(landingpage)/page.tsx
@@ -0,0 +1,68 @@
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
+export default function LandingPage() {
+  return (
+    <div className="mx-auto w-full flex-1 flex-col items-center justify-center gap-2 lg:flex-row">
+      <div className="bg-blue-500 w-full px-32 flex justify-around">
+        <div className="relative mb-8 h-[240px] w-[240px] lg:mb-0 lg:h-[424px] lg:w-[424px]">
+          <Image src="/icon.svg" alt="Di Dau Day" fill />
+        </div>
+
+        <div className="flex flex-col items-center gap-y-8 justify-center">
+          <h1 className="max-w-[424px] text-center text-xl font-bold text-neutral-600 lg:text-2xl">
+            Vietnamese Monopoly Culture Game
+          </h1>
+
+          <h4 className="max-w-[424px] text-center text-xl text-neutral-600 lg:text-sm">Bạn mang một niềm đam mê cháy bỏng dành cho văn hóa Việt Nam? Bạn muốn tìm hiểu di sản một cách thú vị và sáng tạo? Vậy thì “ĐI ĐÂU ĐÂY ?!” chính là dành cho bạn!</h4>
+
+          <div className="flex w-full max-w-[330px] flex-col items-center gap-y-3">
+            <ClerkLoading>
+              <Loader className="h-5 w-5 animate-spin text-muted-foreground" />
+            </ClerkLoading>
+
+            <ClerkLoaded>
+              <SignedOut>
+                <SignUpButton
+                  mode="modal"
+                  fallbackRedirectUrl="/learn"
+                >
+                  <Button size="lg" variant="secondary" className="w-full">
+                    Bắt đầu
+                  </Button>
+                </SignUpButton>
+
+                <SignInButton
+                  mode="modal"
+                  fallbackRedirectUrl="/learn"
+                  signUpFallbackRedirectUrl="/learn"
+                >
+                  <Button size="lg" variant="primaryOutline" className="w-full">
+                    Đăng nhập
+                  </Button>
+                </SignInButton>
+              </SignedOut>
+
+              <SignedIn>
+                <Button size="lg" variant="secondary" className="w-full" asChild>
+                  <Link href="/learn">Tiếp tục học</Link>
+                </Button>
+              </SignedIn>
+            </ClerkLoaded>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
